e678b77945844cc182ffc25f893a7218c7a520a9
KimC <kimcampbel@gmail.com>
2018-12-24 10:27:35 -0500

    updated

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,70 +14,76 @@ with open(csvpath, newline="") as csvfile:
     # create a python list from the date column, assign it variable
     date =[]
     pnl =[]
+    delta=[]
 
     # for each row in 88 loops print the first column of data into a list date and 2nd column in list pnl
     for row in csvreader:
         (date.append(row[0]))
         (pnl.append(row[1]))
         
-    # calculate the length of the list. 
+    #TOTAL MONTHS:  calculate the length of the list. 
     (TotalMonths)=(len(date))
     int(TotalMonths)
     
-    #make values in pnl all ints using list comprehension
+
+    #NET TOTAL: make values in pnl all ints using list comprehension
     pnl = [int (pnl2) for (pnl2) in pnl]
-    # create a sum function or the list 
     sumpnl = int(sum(pnl))
 
-    # NET TOTAL PNL 
-    print(sumpnl)
 
-    #The average of the changes in "Profit/Losses" over the entire period
-    #calculate the average by using the length variable as denominatore & sum as numerator
-    #print 
-    average = sumpnl / TotalMonths
-    #print(average)
+    #AVERAGE CHANGE IN PNL 
+    # create a loop that subtracts index2 from index 1, and adds each difference to a delta list
+    delta = [(pnl[x]) - (pnl[x+1]) for x in range(len(pnl)-1)]
 
-
-#The greatest decrease in profits 
-    #use min to determine the lowest value in P&L list 
-    maxprof=max(pnl)
-    #print ("Highest value in P&L List " + str(maxprof))
-
-    #return the index of that number and set it to variable x 
-    x= pnl.index(maxprof)
-    #print(x)
-
-    #return x index of the Date column to get the Date 
-    highestdate=date[x]
-    # print Date and Profit 
+    #make values in delta all ints using list comprehension
+    delta2= [int(delta2) for delta2 in delta]
+    avgdelta = round(int(sum(delta2)) / (TotalMonths-1), 2)
    
-    minprof=min(pnl)
-    #print ("Lowest value in P&L List " + str(minprof))
-    #return the index of that number and set it to variable x 
-    y= pnl.index(minprof)
-    #print(y)
-    #return x index of the Date column to get the Date 
-    lowestdate=date[y]
-    # print Date and Profit 
+
+    #GREATEST INCREASE 
+    #use max to determine the highest increase in delta 
+    maxprof= max(delta)
+    
+    #return the index of that number and set it to variable y 
+    y= delta.index(maxprof)
+
+    #return y index of the Date column to get the Date 
+    highestdate =date[y]
+
+
+   #GREATEST LOSS
+    #use min to determine the largest decrease in delta 
+    minprof=min(delta)
+
+    #return the index of that number and set it to variable y 
+    z= delta.index(minprof)
+
+    #return y index of the Date column to get the Date 
+    lowestdate =date[z]
+    
+   
     
   
 # output in terminal 
 print("Financial Analysis")
 print("------------------------------")
 print("Total Months: "+ str(TotalMonths))
-print("Average Change: " + "$" + str(average))
-print("Highest date = " + str(highestdate)+ " and highest profit = " + str(maxprof))
-print("Lowest date = " + str(lowestdate)+ " and lowest profit = " + str(minprof))
+print("Net Total: "+ "$"+ str(sumpnl))
+print("Average Change: " + "$" + str(avgdelta))
+print("In " + str(highestdate)+ " we earned the highest profit: " + "$"+ str(maxprof))
+print("In " + str(lowestdate)+ " we suffered the highest loss: " + "$"+ str(minprof))
+print("------------------------------")
 
 #output to text file 
 f = open("pybank1.txt", "w+")
-print("Financial Analysis", file=f)
+print("Financial Analysis",file=f)
 print("------------------------------", file=f)
 print("Total Months: "+ str(TotalMonths), file=f)
-print("Average Change: " + "$" + str(average), file=f)
-print("Highest date = " + str(highestdate)+ " and highest profit = " + str(maxprof), file=f)
-print("Lowest date = " + str(lowestdate)+ " and lowest profit = " + str(minprof), file=f)
+print("Net Total: "+ "$"+ str(sumpnl), file=f)
+print("Average Change: " + "$" + str(avgdelta))
+print("In " + str(highestdate)+ " we earned the highest profit: " + "$"+ str(maxprof), file=f)
+print("In " + str(lowestdate)+ " we suffered the highest loss: " + "$"+ str(minprof), file=f)
+print("------------------------------", file=f)
     
        
     
